e4de2d0aa88c487dcdd0d0869cc155c52d0a7d0f
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-05-21 11:00:04 +0000

    UI変更

--- create.php
+++ create.php
@@ -3,7 +3,7 @@
 $text = $_POST['paste'];
 echo "Get text...";
 $band = chr(mt_rand(97, 122));
-for($i = 0; $i < 10; $i++){
+for($i = 0; $i < 6; $i++){
   $band .= chr(mt_rand(97, 122));
 }
 $filename = $band.".txt";
@@ -11,9 +11,14 @@ if (file_exists($filename)) {
   echo '<br><font color="red">An error has occurred Forcibly reload even if a warning from the browser occurs.</font>';
   exit;
 } else {
+  if( empty($var) ){
+    echo '<br><font color="red">An error has occurred Forcibly reload even if a warning from the browser occurs.</font>';
+      }
+  else{
     echo "<br>Set url band: ".$band;
   file_put_contents($filename, $text);
   echo "<br>Success!";
   echo '<meta http-equiv="refresh" content="0;URL=/'.$filename.'">';
+  }
 }
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,43 @@
 <head>
-  <title>Free Paster (Open source)</title>
+  <meta charset="utf-8">
+  <meta name="google-site-verification" content="tn-ijQJquJQDgR3wlRTB8I2swGjDmTH_3NQdi4VbYBI" />
+<meta name="robots" content="all">
+<meta name="theme-color" content="#525252">
+<meta name="keywords" content="paster, misskey, mastodon, pastebin, opensource, オープンソース, フリー, github, git, txt, 世界初, beta, β">
+<meta name="description" content="おそらく世界初のオープンソースのテキストPasteサービス。">
+<meta property="og:title" content="Paster - フリーでオープンソースのテキストペーストサービス">
+<meta property="og:type" content="article">
+<meta property="og:description" content="おそらく世界初のオープンソースのテキストPasteサービス。">
+<meta property="og:url" content="https://paste.f5.si">
+<meta property="og:image" content="https://paste.f5.si/a.png">
+<meta property="og:site_name" content="Paster - フリーでオープンソースのテキストペーストサービス">
+  <title>Paster v0.1.0</title>
+  <style>
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap');
+body {
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+}
+textarea:focus {
+  border:1px solid #858585;
+  outline:0;
+  box-shadow: 10px 10px 15px -10px;
+}
+textarea {
+  border:1px solid #000000;
+  outline:0;
+  box-shadow: 10px 10px 15px -10px;
+}
+</style>
 </head>
 <body>
-  <h1>Free Paster (Open source)</h1>
+  <center><p><font size="6">Paster <b>β</b></font></p>
   <hr>
   <form action="create.php" method="post">
-    <p>Paste text:</p>
-    <textarea name="paste" cols="50" rows="10"></textarea>
+    <h3>Paste text</h3>
+    <textarea name="paste" cols="80" rows="20"></textarea>
     <p><input type="submit" name="send" value="Create!"></p>
   </form>
+<hr>
+<p>© <a href="moto.php" alt="前のバージョンのURL" title="前のバージョン(ゴミ)"><font color="black">技術力の低さを「軽量化」という言い訳でごまかすプログラミング初心者の鑑</font></a></p>
+</center>
 </body>
